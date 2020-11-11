883fd3fce0cf107906d37a3a6bb8ea53fe2993e7
Rishabh Chadha <rchadha@nvidia.com>
2020-11-11 13:52:40 -0800

    Update docker_run.sh updated display flag

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,2 +1,2 @@
 sudo xhost +si:localuser:root
-sudo docker run -it --rm --runtime nvidia --device="/dev/video0:/dev/video0" --network host -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v ${pwd}:/workdir ros2_torch2trt_base:jp44
+sudo docker run -it --rm --runtime nvidia -e DISPLAY=$DISPLAY --device="/dev/video0:/dev/video0" --network host -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v ${pwd}:/workdir ros2_torch2trt_base:jp44
